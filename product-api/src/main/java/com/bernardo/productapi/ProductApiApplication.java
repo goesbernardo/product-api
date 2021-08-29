62dfd66217e5890b4e7abbd07f1f5a319b432b7e
Bernardo Trindade <b.trindade@reply.com>
2021-08-29 19:52:00 -0300

    first commit

---
+++ product-api/src/main/java/com/bernardo/productapi/ProductApiApplication.java
@@ -0,0 +1,13 @@
+package com.bernardo.productapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductApiApplication.class, args);
+	}
+
+}
